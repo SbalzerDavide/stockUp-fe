e1353d6a597e521f68d4cd8fda2a43dd123e422d
SbalzerDavide <sbalzerdavide@hotmail.it>
2025-03-28 23:45:28 +0100

    Rimuovi gli interceptor di Axios dal client API principale e integra la gestione dei cookie JWT nel client di autenticazione. Assicura che i cookie vengano inviati con le richieste di login e recupero dati utente.

--- features/auth/api/auth-api-client.ts
+++ features/auth/api/auth-api-client.ts
@@ -1,57 +1,28 @@
-import axios from 'axios';
-
-// Configurazione dell'istanza axios
-const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
-  // withCredentials: true, // ensures that Axios sends cookies with requests
-});
-
-// Interceptor per gestire i cookie dalle risposte
-api.interceptors.response.use(
-  async response => {
-    return response;
-  },
-  error => {
-    return Promise.reject(error);
-  },
-);
-
-// Funzione per impostare il token di autenticazione
-export function setAuthToken(token: string | null) {
-  if (token) {
-    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-  } else {
-    delete api.defaults.headers.common['Authorization'];
-  }
-}
+import { api } from '@/core/api-client';
 
 // Funzione per effettuare il login
 export async function loginRequest(email: string, password: string) {
   const response = await api.post('/login', {
     email,
     password,
+  }, {
+    withCredentials: true // Assicuriamoci che i cookie vengano inviati
   });
   
-  // Imposta il token per le future richieste
-  if (response.data.token) {
-    setAuthToken(response.data.token);
-  }
   
   return response.data;
 }
 
 // Funzione per ottenere i dati dell'utente
-export async function fetchUser(token?: string) {
-  if (token) {
-    setAuthToken(token);
-  }
-  
+export async function fetchUser() {
   try {
+    
     const response = await api.get('/me', {
+      withCredentials: true, // Assicuriamoci che i cookie vengano inviati
       headers: {
-        'accept': 'application/json',
+        'Accept': 'application/json',
         'Content-Type': 'application/json',
-      },
+      }
     });
     
     return response.data;
