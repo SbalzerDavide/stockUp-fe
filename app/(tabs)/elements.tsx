3760be02a2f297ee593749a2f757e21b6ada457c
SbalzerDavide <sbalzerdavide@hotmail.it>
2025-04-01 23:11:58 +0200

    feat: add element page

---
+++ app/(tabs)/elements.tsx
@@ -0,0 +1,50 @@
+import { StyleSheet, Image } from "react-native";
+
+
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import { ItemCard } from "@/components/ItemCard";
+
+import React from "react";
+import { useItems } from "@/features/items/api/items.mutations";
+export default function ElementsScreen() {
+  const { data: items, isLoading, error } = useItems();
+
+  return (
+    <>
+      <ParallaxScrollView
+        headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
+        headerImage={
+          <IconSymbol
+            size={310}
+            color="#808080"
+            name="chevron.left.forwardslash.chevron.right"
+            style={styles.headerImage}
+          />
+        }
+      >
+        {items?.map((item: any) => (
+          <ItemCard
+            key={item.id}
+            title={item.name}
+            description={item.description}
+            imageUrl="https://via.placeholder.com/50"
+          />
+        ))}
+      </ParallaxScrollView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: "#808080",
+    bottom: -90,
+    left: -35,
+    position: "absolute",
+  },
+  titleContainer: {
+    flexDirection: "row",
+    gap: 8,
+  },
+});
