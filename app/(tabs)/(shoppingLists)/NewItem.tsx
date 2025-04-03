1a3a27257dd9adc6d41b9b605566024034423891
SbalzerDavide <sbalzerdavide@hotmail.it>
2025-04-03 23:51:07 +0200

    refactor: update NewItem screen layout and improve item creation logic

--- app/(tabs)/(shoppingLists)/NewItem.tsx
+++ app/(tabs)/(shoppingLists)/NewItem.tsx
@@ -1,14 +1,15 @@
 import { Button, ButtonText } from "@/components/ui/button";
 import { Input, InputField } from "@/components/ui/input";
 
-import { useCreateItem } from "@/features/items/api/items.mutations";
+import { useCreateItem } from "@/features/items/api/shoppingList.mutations";
 import { Box } from "@/components/ui/box";
 import { useState } from "react";
 import { View } from "react-native";
 import { Text } from "react-native";
 import React from "react";
 import { useShowToast } from "@/hooks/useShowToast";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
+import { Stack } from "expo-router";
 
 export default function NewItemScreen() {
   const [name, setName] = useState("");
@@ -20,56 +21,67 @@ export default function NewItemScreen() {
 
   const handleCreateItem = async () => {
     setLoading(true);
-    createItem({
-      name,
-    }, {
-      onSuccess: () => {
-        showToast({
-          titleKey: "toasts.success.title",
-          descriptionKey: "toasts.success.itemCreated",
-          action: "success"
-        })
-        setName("")
+    createItem(
+      {
+        name,
       },
-      onError: () => {
-        showToast({
-          titleKey: "toasts.error.title",
-          descriptionKey: "toasts.error.createFailed",
-          action: "error"
-        })
-      },
-      onSettled: () => {
-        setLoading(false);
+      {
+        onSuccess: () => {
+          showToast({
+            titleKey: "toasts.success.title",
+            descriptionKey: "toasts.success.itemCreated",
+            action: "success",
+          });
+          setName("");
+        },
+        onError: () => {
+          showToast({
+            titleKey: "toasts.error.title",
+            descriptionKey: "toasts.error.createFailed",
+            action: "error",
+          });
+        },
+        onSettled: () => {
+          setLoading(false);
+        },
       }
-    });
-
+    );
   };
-  
+
   return (
-    <View className="flex-1">
-      <View className="w-full flex-grow p-5">
-        <Text className="text-2xl font-semibold">{t('pages.newItem.title')}</Text>
-        <Box className="py-5 flex-col gap-2">
-          <Input>
-            <InputField
-              value={name}
-              onChangeText={setName}
-              autoCapitalize="none"
-              placeholder={t('common.labels.enterName')}
-            />
-          </Input>
-        </Box>
-        <Box className="flex flex-col gap-2">
-          <Button
-            action="primary"
-            onPress={handleCreateItem}
-            disabled={loading || isPending}
-          >
-            <ButtonText>{t('common.buttons.newItem')}</ButtonText>
-          </Button>
-        </Box>
+    <>
+      <Stack.Screen
+        name="NewItem"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <View className="flex-1">
+        <View className="w-full flex-grow p-5">
+          <Text className="text-2xl font-semibold">
+            {t("pages.newItem.title")}
+          </Text>
+          <Box className="py-5 flex-col gap-2">
+            <Input>
+              <InputField
+                value={name}
+                onChangeText={setName}
+                autoCapitalize="none"
+                placeholder={t("common.labels.enterName")}
+              />
+            </Input>
+          </Box>
+          <Box className="flex flex-col gap-2">
+            <Button
+              action="primary"
+              onPress={handleCreateItem}
+              disabled={loading || isPending}
+            >
+              <ButtonText>{t("common.buttons.newItem")}</ButtonText>
+            </Button>
+          </Box>
+        </View>
       </View>
-    </View>
+    </>
   );
 }
-
