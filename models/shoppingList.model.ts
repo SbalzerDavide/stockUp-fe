17017fccf486d1f818451e65d670617885f451d1
SbalzerDavide <sbalzerdavide@hotmail.it>
2025-04-18 00:18:08 +0200

    feat: add ShoppingListItem and ShoppingListDetail interfaces to enhance shopping list functionality

--- models/shoppingList.model.ts
+++ models/shoppingList.model.ts
@@ -1,3 +1,5 @@
+import { User } from "./auth.model";
+
 export interface ShoppingList {
   id: number;
   name: string;
@@ -6,6 +8,34 @@ export interface ShoppingList {
   is_purchased: boolean;
   created_at: string;
   updated_at: string;
+}
+
+export interface ShoppingListItem {
+  id: number;
+  item_name: string;
+  item_id: number;
+  is_checked: boolean;
+  is_propposed: boolean;
+  macronutriments: string | null;
+  category: string | null;
+  department: string | null;
+  quantity: number;
+  weight: number;
+  unit_weight: string;
+  volume: number;
+  unit_volume: string;
+}
+
+export interface ShoppingListDetail {
+  id: number;
+  user: User;
+  name: string;
+  description?: string;
+  is_active: boolean;
+  is_purchased: boolean;
+  created_at: string;
+  updated_at: string;
+  items: ShoppingListItem[];
 }
 
 export interface createShoppingListRequest {
