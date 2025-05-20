0ae795161eb7ab9678903d95c7d86daa20ab2285
SbalzerDavide <sbalzerdavide@hotmail.it>
2025-05-20 22:53:42 +0200

    clean routes

--- app/(tabs)/shoppingLists/_layout.tsx
+++ app/(tabs)/shoppingLists/_layout.tsx
@@ -4,26 +4,12 @@ import { Stack } from "expo-router";
 export default function ShoppingListsLayoutScreen() {
   return (
     <>
-      <Stack>
+      <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen
           name="index"
         />
-        {/* <Stack.Screen name="Items" /> */}
-
-        <Stack.Screen
-          name="items"
-          options={{
-            headerShown: false,
-          }}
-        />
         <Stack.Screen
           name="newShoppingList"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="detail"
           options={{
             headerShown: false,
           }}
