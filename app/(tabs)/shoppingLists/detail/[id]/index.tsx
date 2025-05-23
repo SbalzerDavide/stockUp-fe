07a279a69db0f1bedf0a37081e59c9644f03a61d
SbalzerDavide <sbalzerdavide@hotmail.it>
2025-05-24 01:20:23 +0200

    feat: enhance shopping list detail screen with accordion and checkbox components for item selection

--- app/(tabs)/shoppingLists/detail/[id]/index.tsx
+++ app/(tabs)/shoppingLists/detail/[id]/index.tsx
@@ -5,7 +5,13 @@ import { ItemCard } from "@/features/shoppingList/components/ItemCard";
 import { Switch } from "@/components/ui/switch";
 import colors from "tailwindcss/colors";
 import { Text } from "@/components/ui/text";
-import { Plus } from "lucide-react-native";
+import {
+  CheckIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+  ShoppingCart,
+  TableOfContents,
+} from "lucide-react-native";
 
 import React, { useCallback, useEffect } from "react";
 import {
@@ -16,6 +22,8 @@ import {
   useMacronutriments,
   useShoppingList,
 } from "@/features/shoppingList/api/shoppingList.mutations";
+
+import { AddIcon } from "@/components/ui/icon";
 
 import { VStack } from "@/components/ui/vstack";
 import { Box } from "@/components/ui/box";
@@ -39,6 +47,26 @@ import { ThemedView } from "@/components/ThemedView";
 import { useThemeColor } from "@/hooks/useThemeColor";
 import { useShowToast } from "@/hooks/useShowToast";
 import { ItemCardQuantity } from "@/features/shoppingList/components/ItemCardQuantity";
+import { Fab, FabIcon, FabLabel } from "@/components/ui/fab";
+
+import { departments } from "@/assets/mock/departments";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionContentText,
+  AccordionHeader,
+  AccordionIcon,
+  AccordionItem,
+  AccordionTitleText,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Divider } from "@/components/ui/divider";
+import {
+  Checkbox,
+  CheckboxIcon,
+  CheckboxIndicator,
+  CheckboxLabel,
+} from "@/components/ui/checkbox";
 
 interface Filter {
   id: string;
@@ -58,6 +86,7 @@ export default function ShoppingListDetailScreen() {
   const color = useThemeColor({}, "text");
 
   const [isEdible, setIsEdible] = useState(true);
+  const [isInShopping, setIsInShopping] = useState(false);
   const [showActionsheet, setShowActionsheet] = useState(false);
   const [inputText, setInputText] = useState("");
   const [debouncedSearchText, setDebouncedSearchText] = useState("");
@@ -66,6 +95,13 @@ export default function ShoppingListDetailScreen() {
   const [activeFilterIndex, setActiveFilterIndex] = useState<number | null>(
     null
   );
+
+  const [checkedItems, setCheckedItems] = useState<any>(
+    departments.map(() => ({
+    checkedItems: [],
+  }
+)));
+
 
   const { data: macronutriments } = useMacronutriments();
   const { data: itemCategories } = useItemCategories();
@@ -116,9 +152,26 @@ export default function ShoppingListDetailScreen() {
       return [];
     }
     return items?.results?.filter((item) => {
-      return !shoppingList?.items?.some(
-        (listItem) => listItem.id === item.id
+      return !shoppingList?.items?.some((listItem) => listItem.id === item.id);
+    });
+  };
+
+  const itemsByDepartment = (): {
+    department?: string;
+    items?: any[];
+    total?: number;
+    checkedItems: string[];
+  }[] => {
+    return departments.map((department) => {
+      const itemsByDepartment = shoppingList?.items?.filter(
+        (item) => item.department === department
       );
+      return {
+        department: department,
+        items: itemsByDepartment,
+        total: itemsByDepartment?.length,
+        checkedItems: [],
+      };
     });
   };
 
@@ -169,7 +222,11 @@ export default function ShoppingListDetailScreen() {
     }
   };
 
-  const { mutate: createShoppingListitem, isPending: isPendingShoppingListItem, isSuccess: isSuccessShoppingListItem } = useCreateShoppingListItem();
+  const {
+    mutate: createShoppingListitem,
+    isPending: isPendingShoppingListItem,
+    isSuccess: isSuccessShoppingListItem,
+  } = useCreateShoppingListItem();
   const { mutate: createItem, isPending, isSuccess } = useCreateItem();
   const showToast = useShowToast();
 
@@ -177,7 +234,7 @@ export default function ShoppingListDetailScreen() {
     createShoppingListitem(
       {
         shoppingListId: id as string,
-        itemId
+        itemId,
       },
       {
         onSuccess: () => {
@@ -195,12 +252,34 @@ export default function ShoppingListDetailScreen() {
             action: "error",
           });
         },
-        onSettled: () => {
-        },
+        onSettled: () => {},
       }
     );
   };
 
+  const toggleCheckedItem = (
+    departmentIndex: number,
+    itemId: string,
+    value: boolean
+  ) => {
+    const updatedItems = itemsByDepartment()[departmentIndex].checkedItems;
+    console.log(updatedItems);
+    console.log(value);
+    
+    
+    if(value) {
+      itemsByDepartment()[departmentIndex].checkedItems.push(itemId);
+      console.log(itemsByDepartment()[departmentIndex].checkedItems);
+      
+    } else{
+      if (updatedItems.includes(itemId)) {
+        const index = updatedItems.indexOf(itemId);
+        if (index > -1) {
+          itemsByDepartment()[departmentIndex].checkedItems.splice(index, 1);
+        }
+      }
+    }
+  };
 
   const handleCreateItem = async () => {
     createItem(
@@ -209,7 +288,7 @@ export default function ShoppingListDetailScreen() {
       },
       {
         onSuccess: (el) => {
-          handleCreateShoppingListItem(el.id)
+          handleCreateShoppingListItem(el.id);
           showToast({
             titleKey: "toasts.success.title",
             descriptionKey: "toasts.success.itemCreated",
@@ -228,8 +307,6 @@ export default function ShoppingListDetailScreen() {
     );
   };
 
-  
-
   return (
     <>
       <Stack.Screen
@@ -246,107 +323,202 @@ export default function ShoppingListDetailScreen() {
       />
       <ThemedView className="flex-col gap-4 h-full p-4">
         <ScrollView>
-          <Box className="w-full p-4">
-            <Input>
-              <InputField
-                style={{ color }}
-                value={inputText}
-                placeholder={t("common.labels.searchText")}
-                onChangeText={(text) => setInputText(text)}
-              />
-            </Input>
-          </Box>
-
-          <ThemedView className="w-full flex flex-row justify-between py-4">
-            <ScrollView horizontal={true}>
-              <Box className="flex flex-row gap-2 ml-4">
-                {filters?.map((filter, index) => (
-                  <Pressable
-                    key={filter.id}
-                    onPress={openFilter(filter, index)}
-                    className="bg-background-900 rounded-lg"
-                  >
-                    {filter.value != null ? (
-                      <Text className="bg-primary-400 p-2 rounded-lg">
-                        {
-                          filter.options?.find((el) => el.id === filter.value)
-                            ?.name
-                        }
-                      </Text>
-                    ) : (
-                      <Text className="p-2 rounded-lg">{filter.name}</Text>
-                    )}
-                  </Pressable>
-                ))}
-              </Box>
-            </ScrollView>
-          </ThemedView>
-          {/* <ListFilter size={24} /> */}
-          {/* </Box> */}
-
-          <VStack className="gap-4 p-4" reversed={false}>
-            {listItems()?.length > 0 ? (
-              <Text>Already in list</Text>
-            ) : (
-              <Text>No items founded already in list</Text>
-            )}
-
-            {listItems()?.length > 0
-              ? listItems()?.map((item) => (
-                  <ItemCardQuantity
-                    key={item.id}
-                    id={item.id}
-                    title={item.item_name}
-                    quantity={item.quantity}
-                    emoji={item.emoji}
-                    onSelect={() =>
-                      router.push(
-                        `/(tabs)/shoppingLists/detail/${id}/shoppingListItem/${item.id}`
-                      )
-                    }
-                    // macronutrients={item.macronutriments || ''}
-                    // category={item.category || ''}
-                    // department={item.department || ''}
-                  />
-                ))
-              : debouncedSearchText == "" && <Text>No items found</Text>}
+          {!isInShopping && (
             <Box className="w-full p-4">
-              {debouncedSearchText != "" &&
-                items &&
-                items.results?.filter((el) => el.name === debouncedSearchText)
-                  .length === 0 && (
-                  <Pressable
-                    className="bg-primary-400 rounded-lg p-2"
-                    onPress={() => handleCreateItem()}
-                  >
-                    <Text>Add New Item: {debouncedSearchText}</Text>
-                  </Pressable>
-                )}
-            </Box>
-
-            <Text>To add</Text>
-            {debouncedSearchText != "" &&
-            itemsNoAlreadyInList() &&
-            itemsNoAlreadyInList().length > 0 ? (
-              items?.results?.map((item) => (
-                <ItemCard
-                  key={item.id}
-                  id={item.id}
-                  title={item.name}
-                  emoji={item.emoji}
-                  onSelect={() =>
-                    handleCreateShoppingListItem(item.id)
-                  }
-                  // macronutrients={item.macronutriments || ''}
-                  // category={item.category || ''}
-                  // department={item.department || ''}
+              <Input>
+                <InputField
+                  style={{ color }}
+                  value={inputText}
+                  placeholder={t("common.labels.searchText")}
+                  onChangeText={(text) => setInputText(text)}
                 />
-              ))
-            ) : (
-              <Text>No items found</Text>
-            )}
-          </VStack>
+              </Input>
+            </Box>
+          )}
+
+          {!isInShopping ? (
+            <>
+              <ThemedView className="w-full flex flex-row justify-between py-4">
+                <ScrollView horizontal={true}>
+                  <Box className="flex flex-row gap-2 ml-4">
+                    {filters?.map((filter, index) => (
+                      <Pressable
+                        key={filter.id}
+                        onPress={openFilter(filter, index)}
+                        className="bg-background-900 rounded-lg"
+                      >
+                        {filter.value != null ? (
+                          <Text className="bg-primary-400 p-2 rounded-lg">
+                            {
+                              filter.options?.find(
+                                (el) => el.id === filter.value
+                              )?.name
+                            }
+                          </Text>
+                        ) : (
+                          <Text className="p-2 rounded-lg">{filter.name}</Text>
+                        )}
+                      </Pressable>
+                    ))}
+                  </Box>
+                </ScrollView>
+              </ThemedView>
+
+              <VStack className="gap-4 p-4" reversed={false}>
+                {listItems()?.length > 0 ? (
+                  <Text>Already in list</Text>
+                ) : (
+                  <Text>No items founded already in list</Text>
+                )}
+
+                {listItems()?.length > 0
+                  ? listItems()?.map((item) => (
+                      <ItemCardQuantity
+                        key={item.id}
+                        id={item.id}
+                        title={item.item_name}
+                        quantity={item.quantity}
+                        emoji={item.emoji}
+                        onSelect={() =>
+                          router.push(
+                            `/(tabs)/shoppingLists/detail/${id}/shoppingListItem/${item.id}`
+                          )
+                        }
+                        // macronutrients={item.macronutriments || ''}
+                        // category={item.category || ''}
+                        // department={item.department || ''}
+                      />
+                    ))
+                  : debouncedSearchText == "" && <Text>No items found</Text>}
+                <Box className="w-full p-4">
+                  {debouncedSearchText != "" &&
+                    items &&
+                    items.results?.filter(
+                      (el) => el.name === debouncedSearchText
+                    ).length === 0 && (
+                      <Pressable
+                        className="bg-primary-400 rounded-lg p-2"
+                        onPress={() => handleCreateItem()}
+                      >
+                        <Text>Add New Item: {debouncedSearchText}</Text>
+                      </Pressable>
+                    )}
+                </Box>
+
+                <Text>To add</Text>
+                {debouncedSearchText != "" &&
+                itemsNoAlreadyInList() &&
+                itemsNoAlreadyInList().length > 0 ? (
+                  items?.results?.map((item) => (
+                    <ItemCard
+                      key={item.id}
+                      id={item.id}
+                      title={item.name}
+                      emoji={item.emoji}
+                      onSelect={() => handleCreateShoppingListItem(item.id)}
+                      // macronutrients={item.macronutriments || ''}
+                      // category={item.category || ''}
+                      // department={item.department || ''}
+                    />
+                  ))
+                ) : (
+                  <Text>No items found</Text>
+                )}
+              </VStack>
+            </>
+          ) : (
+            <>
+              <Accordion
+                size="sm"
+                variant="filled"
+                type="multiple"
+                isCollapsible={true}
+                isDisabled={false}
+                className="m-5 w-[90%] border border-outline-200"
+              >
+                {itemsByDepartment()?.map((department, index) => (
+                  <>
+                    {department?.items && department.items?.length > 0 && (
+                      <>
+                        <AccordionItem value={index.toString()}>
+                          <AccordionHeader>
+                            <AccordionTrigger>
+                              {({ isExpanded }) => {
+                                return (
+                                  <>
+                                    <AccordionTitleText>
+                                      {department.department} (
+                                      {department.checkedItems.length}/
+                                      {department.total})
+                                    </AccordionTitleText>
+                                    {isExpanded ? (
+                                      <AccordionIcon
+                                        as={ChevronUpIcon}
+                                        className="ml-3"
+                                      />
+                                    ) : (
+                                      <AccordionIcon
+                                        as={ChevronDownIcon}
+                                        className="ml-3"
+                                      />
+                                    )}
+                                  </>
+                                );
+                              }}
+                            </AccordionTrigger>
+                          </AccordionHeader>
+                          <AccordionContent>
+                            <Box className="flex felx-col gap-2">
+                              {department.items?.map((item) => (
+                                <Checkbox
+                                  size="md"
+                                  isInvalid={false}
+                                  isDisabled={false}
+                                  value={""}
+                                  onChange={(value) => {
+                                    toggleCheckedItem(index, item.id, value);
+                                  }}
+                                >
+                                  <CheckboxIndicator>
+                                    <CheckboxIcon as={CheckIcon} />
+                                  </CheckboxIndicator>
+                                  <CheckboxLabel>
+                                    {item.item_name} ({item.quantity})
+                                  </CheckboxLabel>
+                                </Checkbox>
+                              ))}
+                            </Box>
+                          </AccordionContent>
+                        </AccordionItem>
+                        <Divider />
+                      </>
+                    )}
+                  </>
+                ))}
+              </Accordion>
+            </>
+          )}
         </ScrollView>
+        <Fab
+          size="md"
+          placement="bottom right"
+          isHovered={false}
+          isDisabled={false}
+          isPressed={false}
+          onPress={() => setIsInShopping(!isInShopping)}
+        >
+          {isInShopping ? (
+            <>
+              <FabIcon as={TableOfContents} />
+              <FabLabel>Torna alla lista</FabLabel>
+            </>
+          ) : (
+            <>
+              <FabIcon as={ShoppingCart} />
+              <FabLabel>Inizia spesa</FabLabel>
+            </>
+          )}
+        </Fab>
       </ThemedView>
       <Actionsheet isOpen={showActionsheet} onClose={handleClose}>
         <ActionsheetBackdrop />
